26d559a9e2f0ac0d1b32411fa3860a9f2de712b8
Karol Stępień <karol.stepien@me.com>
2019-11-24 15:30:19 +0100

    placeholder data fetching

--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,46 @@
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 import Link from 'next/link';
 
 const PostLink = props => (
-    <li>
-      <Link href="/posts/[id]" as={`/posts/${props.id}`}>
-        <a>{props.id}</a>
-      </Link>
-    </li>
-  );
-  
+  <li>
+    <Link href="/posts/[id]" as={`/posts/${props.id}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
-const Blog = () => {
-    return (
-        <Layout>
-          <h1>My Blog</h1>
-          <ul>
-            <PostLink id="hello-nextjs" />
-            <PostLink id="learn-nextjs" />
-            <PostLink id="deploy-nextjs" />
-          </ul>
-        </Layout>
-      );
+
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>Posts</h1>
+      <ul>
+        {props.posts.map(show => (
+          <li key={show.id}>
+            <PostLink id={`${show.id}`} title={show.name}/>
+            {/* <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link> */}
+          </li>
+        ))}
+        <PostLink id="hello-nextjs" title="Hello" />
+        <PostLink id="learn-nextjs" title="Learn" />
+        <PostLink id="deploy-nextjs" title="Deploy" />
+      </ul>
+    </Layout>
+  );
 }
+
+Blog.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Data fetched. Count: ${data.length}`);
+
+  return {
+    posts: data.map(entry => entry.show)
+  };
+};
 
 export default Blog;
